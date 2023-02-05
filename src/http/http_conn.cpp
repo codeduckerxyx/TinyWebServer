4260234d096e3b43c6a6c811760b45cfef8a6c38
codeduckerxyx <sxc.sb@qq.com>
2023-02-05 19:24:16 +0800

    修改BAD_REQUEST bug

--- src/http/http_conn.cpp
+++ src/http/http_conn.cpp
@@ -319,6 +319,11 @@ http_conn::HTTP_CODE http_conn::process_read()
         }
     }
 
+    if( line_status == LINE_BAD )
+    {
+        return BAD_REQUEST;
+    }
+
     return NO_REQUEST;
 }
 
@@ -327,17 +332,17 @@ http_conn::HTTP_CODE http_conn::do_request()
     strcpy( m_real_file, doc_root );
     int len = strlen( doc_root );
     strncpy( m_real_file + len, m_url, FILENAME_LEN - len - 1 );
-    if ( stat( m_real_file, &m_file_stat ) < 0 )
+    if ( stat( m_real_file, &m_file_stat ) < 0 )    //是否存在该目录或文件夹
     {
         return NO_RESOURCE;
     }
 
-    if ( ! ( m_file_stat.st_mode & S_IROTH ) )
+    if ( ! ( m_file_stat.st_mode & S_IROTH ) )  //文件的访问权限
     {
         return FORBIDDEN_REQUEST;
     }
 
-    if ( S_ISDIR( m_file_stat.st_mode ) )
+    if ( S_ISDIR( m_file_stat.st_mode ) )   //是否是目录
     {
         return BAD_REQUEST;
     }
