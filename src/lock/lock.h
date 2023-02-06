1dd9c66cc50de67c88fe54d340e114b2fac3a1eb
codeduckerxyx <sxc.sb@qq.com>
2023-02-06 10:44:37 +0800

    first commit

--- src/lock/lock.h
+++ src/lock/lock.h
@@ -16,11 +16,6 @@ public:
             throw std::exception();
         }
     }
-    sem( int num ){
-        if( sem_init( &m_sem, 0, num ) != 0 ){
-            throw std::exception();
-        }
-    }
     /* 销毁信号量 */
     ~sem(){
         sem_destroy( &m_sem );
@@ -63,35 +58,36 @@ public:
 /* 封装条件变量的类 */
 class cond{
 private:
+    pthread_mutex_t m_mutex;
     pthread_cond_t m_cond;
 public:
     /* 创建并初始化条件变量 */
     cond(){
+        if( pthread_mutex_init( &m_mutex, NULL ) != 0 ){
+            throw std::exception();
+        }
         if( pthread_cond_init( &m_cond, NULL ) != 0 ){
             /* 构造函数一旦出现问题，就应该立即释放已经成功分配了的资源 */
+            pthread_mutex_destroy( &m_mutex );
             throw std::exception();
         }
     }
     /* 销毁条件变量 */
     ~cond(){
+        pthread_mutex_destroy( &m_mutex );
         pthread_cond_destroy( &m_cond );
     }
     /* 等待条件变量 */
-    bool wait( pthread_mutex_t* m_mutex ){
-        return pthread_cond_wait( &m_cond, m_mutex ) == 0;
+    bool wait(){
+        int ret = 0;
+        pthread_mutex_lock( &m_mutex );
+        ret = pthread_cond_wait( &m_cond, &m_mutex );
+        pthread_mutex_unlock( &m_mutex );
+        return ret == 0;
     }
-
-    bool timewait( pthread_mutex_t* m_mutex, struct timespec t ){
-        return pthread_cond_timedwait( &m_cond, m_mutex, &t ) == 0;
-    }
-
     /* 唤醒等待条件变量的线程 */
     bool signal(){
         return pthread_cond_signal( &m_cond ) == 0;
-    }
-
-    bool broadcast(){
-        return pthread_cond_broadcast( &m_cond ) == 0;
     }
 };
 
